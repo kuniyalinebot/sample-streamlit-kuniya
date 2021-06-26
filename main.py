484aa8ef5120b26cf75d72fdbd330f5e06ad66e8
kuniya <oknuenoik@gmail.com>
2021-06-26 21:33:08 +0900

    1st commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import io
+import requests
+from PIL import Image
+from PIL import ImageDraw
+
+st.title('顔認識アプリ')
+
+subscription_key = '6b2fa27fa58849eb872949db6e047827'
+assert subscription_key
+
+face_api_url = 'https://20200622kuniya.cognitiveservices.azure.com/face/v1.0/detect'
+
+uploaded_file = st.file_uploader("Choose an image...", type='jpg')
+if uploaded_file is not None:
+    img = Image.open(uploaded_file)
+    with io.BytesIO() as output:
+        img.save(output, format="JPEG")
+        binary_img = output.getvalue() #バイナリ取得
+    headers = {
+        'Content-Type': 'application/octet-stream',
+        'Ocp-Apim-Subscription-Key': subscription_key
+    }
+    params = {
+    'returnFaceId': 'true',
+        'returnFaceAttributes': 'age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise',
+    }
+
+    res= requests.post(face_api_url,params=params,headers=headers, data=binary_img)
+
+    results = res.json()
+    for result in results:
+        rect = result['faceRectangle']
+        draw = ImageDraw.Draw(img)
+        draw.rectangle([(rect['left'], rect['top']), (rect['left']+rect['width'], rect['top']+rect['height'])], fill=None, outline='green', width=5)
+
+    st.image(img, caption='uploaded Image,', use_column_width=True)
